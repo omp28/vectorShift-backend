220dbda6fd012c7362f02a55fea0dc162919f9a1
Om Patel <om28patel2004@gmail.com>
2024-11-22 13:57:27 +0530

    fix: using networkX for DAG

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
+import networkx as nx
 
 app = FastAPI()
 
@@ -10,7 +11,7 @@ app.add_middleware(
     allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],  
-    allow_headers=["*"], 
+    allow_headers=["*"],  
 )
 
 class Node(BaseModel):
@@ -36,27 +37,12 @@ def read_root():
 
 @app.post('/pipelines/parse')
 def parse_pipeline(pipeline: Pipeline):
-    m1 = {}
-    mp2 = {}
-    is_dag = True  
-
+    G = nx.DiGraph()  
+    
     for edge in pipeline.edges:
-        start, end = edge.source, edge.target
-        if start not in m1:
-            m1[start] = [end]
-        else:
-            m1[start].append(end)
-
-    for key, values in m1.items():
-        for value in values:
-            if value not in mp2:
-                mp2[value] = 1
-            else:
-                if key in mp2:
-                    is_dag = False
-                    break
-        if not is_dag:
-            break
+        G.add_edge(edge.source, edge.target)
+    
+    is_dag = nx.is_directed_acyclic_graph(G)
 
     num_nodes = len(pipeline.nodes)
     num_edges = len(pipeline.edges)
